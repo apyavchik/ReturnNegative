89583ecdbbe098496daba7493bb53ba3647f3537
apyavchik <81320602+apyavchik@users.noreply.github.com>
2021-04-03 10:36:13 +0300

    add ReturnNegative.java, ReturnNegativeTest.java, README.md

---
+++ src/test/java/ReturnNegativeTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class ReturnNegativeTest {
+
+    @Test
+    public void test1() {
+        assertEquals(-42, ReturnNegative.makeNegative(42));
+    }
+}
